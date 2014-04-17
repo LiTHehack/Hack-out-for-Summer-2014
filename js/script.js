b9136f8ac04139f5e83ff141d6ceb3c24ee84729
tistatos <tistatos@gmail.com>
2014-04-17 14:58:23 +0200

    timeline added and MOAR CATS <3

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ $(document).ready(function(){
 	$('.smoothscroll').smoothScroll();
 
 });
+
+
+$('#eventguide a').click(function (e) {
+  e.preventDefault();
+  $(this).tab('show');
+  console.log(e);
+})
+
+/*$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  var href = $(e.target).attr("href");
+
+
+})*/
