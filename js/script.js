ef4ff6ce32759f95410cd4f2807d149087238cc9
tistatos <tistatos@gmail.com>
2014-04-22 18:53:19 +0200

    added stuff

--- js/script.js
+++ js/script.js
@@ -2,16 +2,3 @@ $(document).ready(function(){
 	$('.smoothscroll').smoothScroll();
 
 });
-
-
-$('#eventguide a').click(function (e) {
-  e.preventDefault();
-  $(this).tab('show');
-  console.log(e);
-})
-
-/*$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-  var href = $(e.target).attr("href");
-
-
-})*/
