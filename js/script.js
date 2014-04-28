a9d6cdaacea7abeb682ba25159105c6b8c60c7a3
Klas Eskilson <klas.eskilson@gmail.com>
2014-04-28 17:49:40 +0200

    Added map. Awesome, if you ask me

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
 	$('.smoothscroll').smoothScroll();
 
+	$('#map').simplegmaps({
+		MapOptions: {
+			center: new google.maps.LatLng(58.589236, 16.179097),
+	        draggable: true,
+	        scrollwheel: false,
+	        streetViewControl: true,
+	        panControl: false,
+	        zoom: 3,
+	        zoomControl: true,
+	        zoomControlOptions: {
+	            style: 'DEFAULT'
+	        }
+	    }
+	});
 
 /*
 	$('.whatTitle').hover(function(){
